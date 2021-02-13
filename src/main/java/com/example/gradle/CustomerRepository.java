ce544d97dcfd2191af732e97ffec147e1500589c
Marara <57908460+marara02@users.noreply.github.com>
2021-02-14 02:06:58 +0600

    Update CustomerRepository.java

--- src/main/java/com/example/gradle/CustomerRepository.java
+++ src/main/java/com/example/gradle/CustomerRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface CustomerRepository extends MongoRepository<Customer, String> {
-    public Customer findByFirstName(String firstName);
-    public List<Customer> findByLastName(String lastName);
+    Customer findByFirstName(String firstName);
+    List<Customer> findByLastName(String lastName);
 }
